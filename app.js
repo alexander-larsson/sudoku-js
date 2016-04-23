c940139a294f0f22a63386839ebbfc6bcaa8fa84
Alexander Larsson <alexx.1992@gmail.com>
2016-04-23 17:29:41 +0200

    Added parsing of file

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
-// Requires
+"use strict";
+
+// ############# Requires ###############
 var fs = require('fs');
 
-// Global vars
+// ############# Global vars ############
 var input_file_path = "";
 
-
-if(process.argv.length === 3){
-  input_file_path = process.argv[2];
+// ############# Main code ##############
+if (process.argv.length === 3) {
+    input_file_path = process.argv[2];
 } else {
-  console.log("Invalid number of arguments.\nUsage: node app.js <file-path>")
-  process.exit(1);
+    console.log("Invalid number of arguments.\nUsage: node app.js <file-path>")
+    process.exit(1);
 }
 
-fs.readFile(input_file_path, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err.stack);
-  }
-  console.log(data);
+fs.readFile(input_file_path, 'utf8', (err, data) => {
+    if (err) {
+        console.log(err.stack);
+        process.exit(1);
+    }
+
+    var rows = data.split(/\r?\n/).splice(0, 9);
+    if (rows.length !== 9) {
+        console.log("Invalid file format, not enough rows");
+        process.exit(1);
+    }
+    var grid = rows.map(rowStringToArray);
+    console.log(grid);
+
 });
+
+// ############# Helpers ################
+
+var rowStringToArray = function(row) {
+    if (row.length !== 9) {
+        console.log("Invalid file format, invalid row length (expected: 9)");
+        process.exit(1);
+    }
+
+    var res = [];
+
+    for (var i = 0; i < row.length; i++) {
+      var currentChar = row.charAt(i);
+      if(currentChar.match(/[1-9]/)){
+        res.push(Number(currentChar));
+      } else {
+        res.push(0);
+      }
+    }
+
+    return res;
+}
